8aaeeb01ceca49379f3a0e764e4b79b64f57a0d1
Hiago Lima <hiago.nascimento@enext.com.br>
2020-02-28 18:15:44 -0300

    desafio lista exercicio

---
+++ js/script.js
@@ -0,0 +1,85 @@
+//Função para evitar comportamento padra do submit
+var botao = document.getElementById('add')
+
+botao.addEventListener('click', function(event){
+	event.preventDefault()
+})
+
+//Recuperação de valores do DOM
+var tempo_entrada = document.getElementById('tempo')
+var exercicio_entrada = document.getElementById('opcoes')
+var data_entrada = document.getElementById('data')
+
+
+//Função que cria e adiciona os valores na lista do DOM
+var criarValores = () => {
+	var tempo_span = document.createElement('span')
+	var tipo_span = document.createElement('span')
+	var data_span = document.createElement('span')
+
+	tempo_span.textContent = tempo_entrada.value + 'h'
+	tipo_span.textContent = exercicio_entrada.value
+	data_span.textContent = data_entrada.value
+
+	var item_div = document.createElement('div')
+	item_div.className = 'lista_itens'
+
+	item_div.appendChild(tempo_span)
+	item_div.appendChild(tipo_span)
+	item_div.appendChild(data_span)
+
+	var lista = document.getElementById('box_lista')
+	lista.appendChild(item_div)
+
+	item_div.appendChild(tempo_span)
+	item_div.appendChild(tipo_span)
+	item_div.appendChild(data_span)
+}
+
+
+//Bloco de códigos para conferir os valores
+var conferirValores = () => {
+	var indicador = 0 //Indica se há erro no preenchimento dos campos
+
+	if(tempo_entrada.value == '' || exercicio_entrada.value == '' || data_entrada.value == ''){
+		indicador = 1
+		avisoErroSuceso(indicador)
+	}
+
+	else{
+		avisoErroSuceso(indicador)
+		criarValores()
+		limparCampos()
+	}
+}
+
+//Função que manipula a caixa de aviso baseado nos valores
+var avisoErroSuceso = (indicador) => {
+	var alerta = document.getElementById('aviso')
+	var alerta_txt = document.getElementById('aviso_txt')
+
+	if(indicador == 1){
+		alerta.style.opacity = '1'
+		alerta_txt.style.background = 'rgba(255,0,0, 0.6)'
+		alerta_txt.textContent = 'Todos os campos devem ser preenchidos'
+		setTimeout(function(){
+			alerta.style.opacity = '0'
+		}, 3000)
+	}
+	else{
+		alerta.style.opacity = '1'
+		alerta_txt.style.background = 'rgba(0,128,0, 0.6)'
+		alerta_txt.textContent = 'Item Adionado com sucesso'
+		setTimeout(function(){
+			alerta.style.opacity = '0'
+		}, 2000)
+	}
+}
+
+//Função que limpa valores dos inputs
+
+var limparCampos = () =>{
+	tempo_entrada.value = ''
+	exercicio_entrada.value = ''
+	data_entrada.value = ''
+}
